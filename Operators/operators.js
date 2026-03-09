c22d537c6cef6965545b80a0e25d0e3c603770d0
Venkateshprasad6 <141996603+Venkateshprasad6@users.noreply.github.com>
2026-03-09 14:17:28 +0530

    Add files via upload

---
+++ Operators/operators.js
@@ -0,0 +1,11 @@
+const number = 23;
+console.log(typeof number)
+const now = 2050;
+const birthYear = now - 2025;
+console.log(birthYear);
+const firstName = "VENKATESH";
+const lastName = "PRASAD";
+console.log(`${firstName} ${lastName}`);
+
+
+
